8e5e47b9476be799367f11fd2ae098ba74433276
Tarayou <kirara0864@gmail.com>
2026-01-15 18:40:04 +0900

    Temporarily disable revenue fetch

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,22 +12,7 @@ import { formatCurrency } from './utils';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
-  try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
-
-    return data;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch revenue data.');
-  }
+  return [];
 }
 
 export async function fetchLatestInvoices() {
