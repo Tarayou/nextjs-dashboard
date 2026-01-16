0d5c08144285bfb51702467502775f1e22d4f086
Tarayou <kirara0864@gmail.com>
2026-01-16 10:41:27 +0900

    Chapter7: dashboard data fetching

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -5,8 +5,8 @@ import { Revenue } from '@/app/lib/definitions';
 
 // This component is representational only.
 // For data visualization UI, check out:
-// https://www.tremor.so/
-// https://www.chartjs.org/
+//https://www.tremor.so/
+//https://www.chartjs.org/
 // https://airbnb.io/visx/
 
 export default async function RevenueChart({
@@ -29,7 +29,7 @@ export default async function RevenueChart({
         Recent Revenue
       </h2>
      
-      { <div className="rounded-xl bg-gray-50 p-4">
+       <div className="rounded-xl bg-gray-50 p-4">
         <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
           <div
             className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
@@ -58,7 +58,7 @@ export default async function RevenueChart({
           <CalendarIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Last 12 months</h3>
         </div>
-      </div> }
+      </div> 
     </div>
   );
 }
